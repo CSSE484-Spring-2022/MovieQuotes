e9357542e3f38dc2821fba7c229a6deb891c4c62
David Fisher <fisherds@rose-hulman.edu>
2022-03-28 08:12:06 -0400

    Initial Commit

---
+++ MovieQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieQuotes
+//
+//  Created by David Fisher on 3/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
