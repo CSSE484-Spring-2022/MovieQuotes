946320662abaeef9490420fc2cb8ae6a0bb8fc90
David Fisher <fisherds@rose-hulman.edu>
2022-03-28 09:19:00 -0400

    function list in the table view

---
+++ MovieQuotes/MovieQuote.swift
@@ -0,0 +1,18 @@
+//
+//  MovieQuote.swift
+//  MovieQuotes
+//
+//  Created by David Fisher on 3/28/22.
+//
+
+import Foundation
+
+class MovieQuote {
+    var quote: String
+    var movie: String
+    
+    init(quote: String, movie: String) {
+        self.quote = quote
+        self.movie = movie
+    }
+}
--- MovieQuotes/MovieQuotesTableViewController.swift
+++ MovieQuotes/MovieQuotesTableViewController.swift
@@ -7,10 +7,19 @@
 
 import UIKit
 
+class MovieQuoteTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    @IBOutlet weak var movieLabel: UILabel!
+    
+}
+
 class MovieQuotesTableViewController: UITableViewController {
     
     let kMovieQuoteCell = "MovieQuoteCell"
-    let names = ["Dave", "Kristy", "McKinley", "Keegan", "Bowen", "Neala"]
+//    let names = ["Dave", "Kristy", "McKinley", "Keegan", "Bowen", "Neala"]
+    var movieQuotes = [MovieQuote]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,21 +29,33 @@ class MovieQuotesTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        // Hardcode some movie quotes
+        let mq1 = MovieQuote(quote: "I'll be back", movie: "The Terminator")
+        let mq2 = MovieQuote(quote: "Yo Adrian", movie: "Rocky")
+        movieQuotes.append(mq1)
+        movieQuotes.append(mq2)
     }
     
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return movieQuotes.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: kMovieQuoteCell, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: kMovieQuoteCell, for: indexPath) as! MovieQuoteTableViewCell
 
         // Configure the cell...
 //        cell.textLabel?.text = "This is row \(indexPath.row)"
-        cell.textLabel?.text = names[indexPath.row]
+//        cell.textLabel?.text = names[indexPath.row]
+        
+//        cell.textLabel?.text = movieQuotes[indexPath.row].quote
+//        cell.detailTextLabel?.text = movieQuotes[indexPath.row].movie
 
+        cell.quoteLabel.text = movieQuotes[indexPath.row].quote
+        cell.movieLabel.text = movieQuotes[indexPath.row].movie
+        
         return cell
     }
 
